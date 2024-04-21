131a097f85ccf798d78d015ce0836ec123e0f622
Askeran17 <m.ataev@mail.ru>
2024-04-21 15:57:30 +0000

    add clouinary

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
+def profile_page(request):
+    user = get_object_or_404(User, user=request.user)
+    comments = user.commenter.all()
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -12,6 +16,7 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name="blog_posts"
     )
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField(default='')
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
